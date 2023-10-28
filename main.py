ffa43777752f8c0d5bd34f3cff383d8443083c4e
paolocs110 <pciappetta@students.avc.edu>
2023-10-27 23:07:30 -0700

    complete assignment 1

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ def main():
     males = int(input("Enter the number of males: "))
     females = int(input("Enter the numer of females: "))
 
-    total = males+females
-    m_perc = f"{males/total:.2%}"
-    f_perc = f"{females/total:.2%}"
+    total = males + females
+    m_perc = males / total * 100
+    f_perc = females / total * 100
 
     print(f"The total number of students: \t{total}")
     print(f"The number of males and females \t {males} \t {females}")
-    print(f"The percentage of males and females \t {m_perc} \t {f_perc}")
+    print(f"The percentage of males and females \t {m_perc:.2f} \t {f_perc:.2f}")
     return m_perc, f_perc
 
 
